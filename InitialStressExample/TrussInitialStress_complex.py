e3af47918416b4ea88974bb007668f777bfba577
zjkl19 <eamdfan@126.com>
2017-09-02 14:05:01 +0800

    add more py files about initial stress

--- InitialStressExample/TrussInitialStress_complex.py
+++ InitialStressExample/TrussInitialStress_complex.py
@@ -25,7 +25,9 @@ session.journalOptions.setValues(replayGeometry=COORDINATE,recoverGeometry=COORD
 
 l1=1.0	#the length of the truss in the bottom. unit:m
 
+l2=1.0	#the length of the truss int the left. unit:m
 
+l3= (l1**2+l2**2)**0.5	#the length of the slope truss. unit:m
 
 modelName='TrussInitialStressModel'     #the name of the model
 
@@ -54,7 +56,15 @@ trussPart1 = myModel.Part(name='trussPart1', dimensionality=THREE_D, type=DEFORM
 # Create the part's base feature
 trussPart1.BaseWire(sketch=sketch1)
 
+sketch2 = myModel.ConstrainedSketch(name='trussSketch2',sheetSize=10.0)
+sketch2.Line(point1=(0.0, 0.0), point2=(0.0, l2))
+trussPart2 = myModel.Part(name='trussPart2', dimensionality=THREE_D, type=DEFORMABLE_BODY)
+trussPart2.BaseWire(sketch=sketch2)
 
+sketch3 = myModel.ConstrainedSketch(name='trussSketch3',sheetSize=10.0)
+sketch3.Line(point1=(0.0, l2), point2=(l1, 0.0))
+trussPart3 = myModel.Part(name='trussPart3', dimensionality=THREE_D, type=DEFORMABLE_BODY)
+trussPart3.BaseWire(sketch=sketch3)
 #-----------------------------------------------------
 
 from material import *
@@ -65,7 +75,7 @@ from material import *
 #It(umat) seems to be no use in the situation in the "integration=BEFORE_ANALYSIS"
 
 #Ematerial=3.45e10   #Young's module
-Ematerial=3.45e10   #Young's module
+Ematerial=1.0   #Young's module
 pmaterial=0.3   #possion ratio
 myTrussMaterial=myModel.Material(name='trussMaterial')
 myModel.materials['trussMaterial'].Elastic(table=((Ematerial, pmaterial), ))
@@ -93,6 +103,29 @@ trussPart1.assignBeamSectionOrientation(method=
     edges=trussPart1.edges.findAt(((0.0, 0.0, 0.0),
     ), ((l1, 0.0, 0.0), ), )))
 
+###
+trussRegion=regionToolset.Region(edges=trussPart2.edges)
+
+trussPart2.SectionAssignment(region=trussRegion, sectionName='trussSection',
+    offset=0.0, offsetField='',offsetType=MIDDLE_SURFACE,
+	thicknessAssignment=FROM_SECTION)
+	
+trussPart2.assignBeamSectionOrientation(method=
+    N1_COSINES, n1=(0.0, 0.0, -1.0), region=Region(
+    edges=trussPart2.edges.findAt(((0.0, 0.0, 0.0),
+    ), ((0.0, l2, 0.0), ), )))
+
+###
+trussRegion=regionToolset.Region(edges=trussPart3.edges)
+
+trussPart3.SectionAssignment(region=trussRegion, sectionName='trussSection',
+    offset=0.0, offsetField='',offsetType=MIDDLE_SURFACE,
+	thicknessAssignment=FROM_SECTION)
+	
+trussPart3.assignBeamSectionOrientation(method=
+    N1_COSINES, n1=(0.0, 0.0, -1.0), region=Region(
+    edges=trussPart3.edges.findAt(((0.0, l1, 0.0),
+    ), ((l2, 0.0, 0.0), ), )))
 #-------------------------------------------------------
 
 from assembly import *
@@ -104,8 +137,68 @@ myAssembly.DatumCsysByDefault(CARTESIAN)
 trussInstance1 = myAssembly.Instance(name='trussInstance1',
     part=trussPart1, dependent=ON)
 
+trussInstance2 = myAssembly.Instance(name='trussInstance2',
+    part=trussPart2, dependent=ON)
 
+trussInstance3 = myAssembly.Instance(name='trussInstance3',
+    part=trussPart3, dependent=ON)
 
+###    
+myModel.ConnectorSection(name='ConnSect-1', 
+    translationalType=JOIN)
+    
+v1 = myAssembly.instances['trussInstance1'].vertices
+v2 = myAssembly.instances['trussInstance2'].vertices
+
+wire = myAssembly.WirePolyLine(points=((v1.findAt(coordinates=(0.0, 0.0, 0.0)), 
+    v2.findAt(coordinates=(0.0, 0.0, 0.0))), ), mergeType=IMPRINT, 
+    meshable=False)
+    
+oldName = wire.name
+myAssembly.features.changeKey(fromName=oldName, 
+    toName='Wire-1')
+
+e1 = myAssembly.edges
+edges1 = e1.findAt(((0.0, 0.0, 0.0), ))
+myAssembly.Set(edges=edges1, name='Wire-1-Set-1')
+region = myAssembly.sets['Wire-1-Set-1']
+csa = myAssembly.SectionAssignment(sectionName='ConnSect-1', region=region)
+
+###
+v1 = myAssembly.instances['trussInstance1'].vertices
+v2 = myAssembly.instances['trussInstance3'].vertices
+
+wire = myAssembly.WirePolyLine(points=((v1.findAt(coordinates=(l1, 0.0, 0.0)), 
+    v2.findAt(coordinates=(l1, 0.0, 0.0))), ), mergeType=IMPRINT, 
+    meshable=False)
+    
+oldName = wire.name
+myAssembly.features.changeKey(fromName=oldName, 
+    toName='Wire-2')
+
+e1 = myAssembly.edges
+edges1 = e1.findAt(((l1, 0.0, 0.0), ))
+myAssembly.Set(edges=edges1, name='Wire-2-Set-1')
+region = myAssembly.sets['Wire-2-Set-1']
+csa = myAssembly.SectionAssignment(sectionName='ConnSect-1', region=region)
+
+###
+v1 = myAssembly.instances['trussInstance2'].vertices
+v2 = myAssembly.instances['trussInstance3'].vertices
+
+wire = myAssembly.WirePolyLine(points=((v1.findAt(coordinates=(0.0, l2, 0.0)), 
+    v2.findAt(coordinates=(0.0, l2, 0.0))), ), mergeType=IMPRINT, 
+    meshable=False)
+    
+oldName = wire.name
+myAssembly.features.changeKey(fromName=oldName, 
+    toName='Wire-3')
+
+e1 = myAssembly.edges
+edges1 = e1.findAt(((0.0, l2, 0.0), ))
+myAssembly.Set(edges=edges1, name='Wire-3-Set-1')
+region = myAssembly.sets['Wire-3-Set-1']
+csa = myAssembly.SectionAssignment(sectionName='ConnSect-1', region=region)  
 
 
 #-------------------------------------------------------
@@ -148,8 +241,8 @@ myModel.DisplacementBC(name='BC-2', createStepName='InitialStep',
 
 
 
-e1 = trussInstance1.edges
-edges1 = e1.findAt(((l1/2, 0.0, 0.0), ))
+e1 = trussInstance2.edges
+edges1 = e1.findAt(((0.0, l2/2, 0.0), ))
 region = myAssembly.Set(edges=edges1, name='Set-2')
 myModel.Stress(name='Predefined Field-1', 
     region=region, distributionType=UNIFORM, sigma11=1.0, sigma22=0, 
@@ -166,7 +259,7 @@ import mesh
 
 
 # Seed the part instance.
-trussPart1.seedPart(size=l1/5,
+trussPart1.seedPart(size=l1,
     deviationFactor=0.1, minSizeFactor=0.1)
     
 elemType=mesh.ElemType(elemCode=T3D2)
@@ -187,6 +280,30 @@ trussPart1.setElementType(regions=pR, elemTypes=(elemType,))
 
 # Mesh the part instance.
 trussPart1.generateMesh()
+
+trussPart2.seedPart(size=l2,
+    deviationFactor=0.1, minSizeFactor=0.1)
+    
+elemType=mesh.ElemType(elemCode=T3D2)
+
+pR=(trussPart2.edges,)
+
+trussPart2.setElementType(regions=pR, elemTypes=(elemType,))
+
+# Mesh the part instance.
+trussPart2.generateMesh()
+
+trussPart3.seedPart(size=l3,
+    deviationFactor=0.1, minSizeFactor=0.1)
+    
+elemType=mesh.ElemType(elemCode=T3D2)
+
+pR=(trussPart3.edges,)
+
+trussPart3.setElementType(regions=pR, elemTypes=(elemType,))
+
+# Mesh the part instance.
+trussPart3.generateMesh()
 
 #-------------------------------------------------------
 
